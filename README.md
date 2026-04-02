889666db8eb6f6b7fa84ed7910fa772f2689765b
Yan Pan <yan@yan.fi>
2026-04-02 20:12:40 +0000

    structuring code

--- README.md
+++ README.md
@@ -9,12 +9,10 @@ There are two flavors provided
 ### Hybrid mode: Local Browser + Container Python Runtime
 
 ```
+# any reachable web
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222 --user-data-dir=$HOME/Lab/z-debug-profile
 
-# /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome \
---remote-debugging-port=9222 \
---user-data-dir=$HOME/Lab/z-debug-profile
-
-# "C:\Program Files\Google\Chrome\Application\chrome.exe" --remote-debugging-port=9222 --user-data-dir="C:\temp\chrome_debug" --no-first-run
+"C:\Program Files\Google\Chrome\Application\chrome.exe" --remote-debugging-port=9222 --user-data-dir="C:\temp\chrome_debug" --no-first-run
 
 # chrome://inspect/#remote-debugging
 # localhost:92222/json/version
---
+++ src/browser_automation/main.py
@@ -0,0 +1,43 @@
+from asyncio import run
+from os import getenv
+
+from browser_use import Agent, Browser, ChatGoogle, ChatOllama, ChatOpenAI
+from browser_use.llm import ChatDeepSeek
+from utils import get_browser_ip
+
+LLM_OPTIONS = {
+    "ollama": ChatOllama(model="llama3.1"),
+    "openai": ChatOpenAI(model="gpt-5.4-nano"),
+    "gemini": ChatGoogle(model="gemini-2.5-flash"),
+    "deepseak": ChatDeepSeek(api_key=getenv("DEEPSEAK_API_KEY")),
+}
+
+TASK = """
+1. Visit page https://hok-elanto.fi/s-etukortti/ajankohtaista-asiakasomistajalle
+2. Check bonus doubled (bonus tuplana) section on the page
+3. Determine campaign offerings and their validity
+4. Summarize in json format with shop short name and date
+"""
+
+TASK = """
+1. Find official Marriot Bonvoy website for Westin Bora Bora Resort & Spa
+2. Check low price calendar for November 2026, 4 nights, 2 adults, using "flexible dates" option from the website
+3. Summarize the lowest price in json format with hotel short name, date and price
+4. Further check rate details for the lowest (and earliest if on-par) date, find half-board price for the entry room type and report
+"""
+
+
+async def run_task():
+    """entry point"""
+
+    agent = Agent(
+        task=TASK,
+        llm=LLM_OPTIONS["deepseak"],
+        browser=Browser(cdp_url=get_browser_ip()),
+    )
+    history = await agent.run()
+    print(history.final_result())
+
+
+if __name__ == "__main__":
+    run(run_task())
